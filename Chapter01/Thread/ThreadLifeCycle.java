c8369ccc1643bface864e4655de0ba35472273c3
akanshakumari1x <wishesakanshagupta918@gmail.com>
2025-12-22 23:10:58 +0530

    ThreadLifeCycle

---
+++ Chapter01/Thread/ThreadLifeCycle.java
@@ -0,0 +1,40 @@
+public class ThreadLifeCycle {
+
+    //providing the implementation of the Runnable interface using a lambda expression.
+ public static void main(String[] args) throws InterruptedException {
+//   1.NEW State
+
+//        Thread t = new Thread(() -> {
+//            System.out.println("Thread running");
+//        });
+//        System.out.println("State: " + t.getState());
+
+//   2. Runnable State
+
+//        Thread t1 = new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//              System.out.println("Thread runnable");
+//            }
+//        });
+//        t1.start();
+//        System.out.println("State: " + t1.getState());
+
+
+//    3. TIMED_WAITING
+
+//                Thread t = new Thread(() -> {
+//                    try {
+//                        Thread.sleep(50000);
+//                    } catch (InterruptedException e) {
+//
+//                    }
+//                });
+//                t.start();
+//                Thread.sleep(1000);
+//                System.out.println("State: " + t.getState());
+
+
+            }
+        }
+
