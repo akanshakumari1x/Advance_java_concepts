883af6321b8c8117c9861fd7d46de9647e95675b
akanshakumari1x <wishesakanshagupta918@gmail.com>
2026-01-07 23:54:30 +0530

    addedNewEx

--- Chapter02/StreamApi/Example1.java
+++ Chapter02/StreamApi/Example1.java
@@ -1,10 +1,13 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Example1 {
    /* Stream API is used to process collections in a functional, declarative, and
     lazy manner using filter, map, reduce, and collect operations.*/
     public static void main(String []arg){
+      //1.  Print even numbers from a list
+
         List<Integer> numbers = Arrays.asList(10, 15, 20, 25, 30);
         numbers.stream()
                 .filter(n -> n % 2 == 0)
@@ -13,6 +16,34 @@ public class Example1 {
 //        stream() → creates stream
 //        filter() → intermediate operation
 //        forEach() → terminal operation
+
+//  2. Get doubled even numbers into a new list
+
+        List<Integer> result =
+                numbers.stream()
+                        .filter(n -> n % 2 == 0)
+                        .map(n -> n * 2)
+                        .toList();
+
+
+      // 3. Find first number greater than 20
+
+        Optional<Integer> value =
+                numbers.stream()
+                        .filter(n -> n > 20)
+                        .findFirst();
+
+        value.ifPresent(System.out::println);
+
+
+       //4. Flatten list of lists
+        List<List<String>> data =
+                List.of(List.of("Java", "Spring"),
+                        List.of("SQL", "Docker"));
+
+        data.stream()
+                .flatMap(List::stream)
+                .forEach(System.out::println);
 
 
 
