e75b90e00c16d0d36bbe73d684fc3221759d4b58
akanshakumari1x <wishesakanshagupta918@gmail.com>
2026-01-28 23:21:58 +0530

    addedExample

--- Chapter02/ReflectionApi/Example1.java
+++ Chapter02/ReflectionApi/Example1.java
@@ -1,5 +1,9 @@
+import java.lang.reflect.Field;
+
 public class Example1 {
 
+
+    // Getting class info here
     Class<?> c = Class.forName("java.lang.String");
 
     public Example1() throws ClassNotFoundException {
@@ -7,6 +11,30 @@ public class Example1 {
         System.out.println(c.getName());       // java.lang.String
         System.out.println(c.getMethods());
     }
+
+
+    // Accessing private field Reflection can break encapsulation.
+    class User {
+        private String name = "Akansha";
+    }
+
+    User u = new User();
+    Field f = u.getClass().getDeclaredField("name");
+    f.setAccessible(true);  // bypass private
+    System.out.println(f.get(u));  // Akansha
+
+
+    //calling method dynamically
+    
+    Method m = u.getClass().getMethod("toString");
+    Object result = m.invoke(u);
+    System.out.println(result);
+
+
+
+
+
+
 
 
 }
